6044a52247939720cfa858aebd69bc4a6ece4dd3
Janekss <97735641+Janekss@users.noreply.github.com>
2022-01-14 15:01:12 +0200

    console loggo

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
-     function nolasa(){
-   
-    const malas=document.getElementsByClassName("mala");
-      
-    const mala1 = parseFloat (m1.value);  //ievadīto informāciju atgriež kā decimālu skaitli
-    const mala2 = parseFloat (m2.value);
-    const mala3 = parseFloat (m3.value);
-      
-      if(mala1>0&&mala2>0&&mala3>0){//pārbauda vai malu gaurmi ir lielāki par 0
-        console.log({mala1,mala2,mala3});
-       return (mala1,mala2,mala3);
-     }else{
-       console.log(false);
+function nolasa() {
+    console.log("Nolasa")
+    const malas = document.getElementsByClassName("mala");
+    const m1 = parseFloat(mala1.value);
+    const m2 = parseFloat(mala2.value);
+    const m3 = parseFloat(mala3.value);
+    if (m1 > 0 && m2 > 0 && m3 > 0) {
+      console.log({m1,m2,m3});
+      return {m1,m2,m3};
+    } else {
+      console.log(false);
       return false;
-    
+    }
+  }
+
+
+   function irTrijsturis(mala1,mala2,mala3){
+  console.log("Vai ir trijsturis?");
+
+  const m= nolasa();
+  m1= m.m1; m2=m.m2; m3=m.m3;
+
+ console.log(m1,m2,m3);
+
+         if (m1<m2+m3 && m2<m1+m3 && m3<m2+m1)  {
+            
+             console.log(true);
+             return true;
+         }else{
+             console.log(false);
+             return false;
+         }
      }
-       
-    }
-    
-    
-    function perimetrs(mala1,mala2,mala3) {
-     const m= nolasa();
-      mala1= m.mala1; mala2=m.mala2; mala3=m.mala3;
-     console.log(mala1,mala2,mala3);
-     const p= mala1 + mala2 + mala3 ;
-      console.log(p)
-     return(p);
-      
-    }
-    
-    
+
+
+  function perimetrs(m1,m2,m3){
+    const m = nolasa();
+    m1=m.m1;m2=m.m2; m3=m.m3;  
+    console.log(m1,m2,m3);
+  const p=m1+m2+m3;
+  console.log(p)
+  return p;
+}
+
+
     function calculate() {
+        console.log("Rēķina");
       
-      
-      const malas=document.getElementsByClassName("mala");
-      
-    const mala1 = parseFloat (m1.value);  //ievadīto informāciju atgriež kā decimālu skaitli
-    const mala2 = parseFloat (m2.value);
-    const mala3 = parseFloat (m3.value);
-      
-    const pusp = (mala1 + mala2 + mala3) * 0.5;
-    const area = Math.sqrt(pusp * (pusp - mala1) * (pusp - mala2) * (pusp - mala3));
+        const m= nolasa();
+        m1= m.m1; m2=m.m2; m3=m.m3;
+
+    const pusp = (m1 + m2 + m3) * 0.5;
+   console.log(pusp);
+    
+    const area = Math.sqrt(pusp * (pusp - m1) * (pusp - m2) * (pusp - m3));
     
       console.log (area);
       return area;
     }
     
-    
-    
-    
-    
-    
-    
+
+
+      
